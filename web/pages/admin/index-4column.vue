0d930dd0f1a50f674009e27fcfe75c4c134b1623
sban <sban@sbandeMacBook-Air.local>
2017-11-16 21:00:41 +0800

    2017-11-16:

--- web/pages/admin/index-4column.vue
+++ web/pages/admin/index-4column.vue
@@ -1,8 +1,70 @@
 <template>
-  <div>
+  <div class="pr-4">
     <div>
       <p class="lead mb-1"><i class="el-icon-arrow-down"></i>&nbsp;四栏内容</p>
       <p class="small text-muted pl-4">选择后编辑</p>
+    </div>
+    <div class="pl-4">
+      <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%">
+        </el-table-column>
+        <el-table-column
+          label="日期"
+          width="180">
+          <template slot-scope="scope">
+            <i class="el-icon-check"></i>&nbsp;
+            <span>{{ scope.row.date }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="姓名">
+          <template slot-scope="scope">
+            {{ scope.row.name }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">隐藏</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="pt-3">
+        <el-button type="success">添加新项</el-button>
+      </div>
+    </div>
+    <div class="pt-4">
+      <p class="lead mb-1"><i class="el-icon-arrow-down"></i>&nbsp;编辑单项</p>
+      <p class="small text-muted pl-4">无操作说明</p>
+    </div>
+    <div class="pl-4">
+      <el-form style="max-width:1080px;" ref="form" :model="form" label-width="80px">
+        <el-form-item label="栏目名称">
+          <el-input style="max-width:300px" v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="图标">
+          <el-input  style="max-width:300px" v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="图片">
+          <img src="~assets/220x220.svg" class="img-thumbnail">
+        </el-form-item>
+        <el-form-item label="标题">
+          <el-input  style="max-width:300px" v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="摘要">
+          <el-input  style="max-width:300px" v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item class="pt-2">
+          <el-button type="primary">保存</el-button>
+          <el-button type="text">取消</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
@@ -17,12 +79,42 @@ export default {
     ],
     link: [
       { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css', integrity: 'sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb', crossorigin: 'anonymous' }
+    ],
+    script: [
+      { src: '/static/js/holder-v2.9.0.min.js', type: 'text/javascript' }
     ]
   },
   methods: {
   },
   data () {
     return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
       formInline: {
         user: '',
         region: ''
@@ -35,4 +127,5 @@ export default {
 </script>
 
 <style>
+
 </style>
